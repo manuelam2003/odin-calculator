6d14c3e0769f2091bfd60f0bb880a081a53540e4
Manuel <manuel.am2003@gmail.com>
2022-05-08 22:52:26 +0200

    Defined calculator function, clearScreen, added style changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 
 function substract(a, b) {
@@ -27,11 +27,39 @@ function operate(operator, a, b) {
   }
 }
 
+function clearScreen() {
+  screen.innerHTML = "0";
+}
+
+function calculator2(input) {
+  let opArr = [];
+  let newInput = input;
+  for (let i = 0; i < input.length; i++) {
+    visor = input[i];
+    if (opSymbols.includes(input[i])) {
+      opArr.unshift(input[i]);
+    }
+    if (opArr.length == 2) {
+      let operation = opArr.pop();
+      [a, rest] = newInput.split(operation);
+      b = rest.split(opArr[0])[0]; // unsure if to use opArr[0] since its length is 1
+      newInput = operate(operation, a, b) + input.slice(i);
+    }
+  }
+  if (opArr) {
+    operation = opArr.pop();
+    [a, b] = newInput.split(operation);
+    newInput = operate(operation, a, b);
+  }
+  screen.innerHTML = Math.round((newInput + Number.EPSILON) * 100) / 100;
+}
+
+const opSymbols = ["+", "-", "*", "/"];
 const screen = document.querySelector("#screen");
 const numButtons = Array.from(document.querySelectorAll(".numBtn"));
 const opButtons = Array.from(document.querySelectorAll(".opBtn"));
 const clearBtn = document.querySelector("#clear");
-const loadBtn = document.querySelector("#load")
+const equalBtn = document.querySelector("#equal");
 
 numButtons.forEach((item) => {
   item.addEventListener("click", () => {
@@ -44,9 +72,12 @@ opButtons.forEach((item) => {
     screen.innerHTML += `${item.innerHTML}`;
   });
 });
-clearBtn.addEventListener("click", () => {
-  screen.innerHTML = "";
-});
+// clearBtn.addEventListener("click", () => {
+//   screen.innerHTML = "0";
+// });
 
-loadBtn.onclick = () => console.log(screen.innerHTML)
+clearBtn.onclick = () => clearScreen()
 
+equalBtn.onclick = () => calculator2(screen.innerHTML);
+
+// include function where it prompts equal after clicking operation if input already contains one
